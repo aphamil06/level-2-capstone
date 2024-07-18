5c9d49dabe7c2fa7fe173056fadee3f3336b2983
Alexander Hamilton <aphamil06@gmail.com>
2024-07-18 00:02:49 -0500

    added function with return value

--- index.js
+++ index.js
@@ -1,11 +1,55 @@
-function redirect(){
-    let answer = prompt("Would you like to book a shoot Now?");
-    if (answer === "yes") {window.location.href="contact.html";
-        alert("Please complete form to begin the booking process");
-    }
-    else {window.location.href="gallery.html";
-        alert ("Feel free to browse our gallery");
-    }
-  
+function redirect() {
+  let answer = prompt("Would you like to book a shoot Now?");
+  if (answer === "yes") {
+    window.location.href = "contact.html";
+    alert("Please complete form to begin the booking process");
+  } else {
+    window.location.href = "gallery.html";
+    alert("Feel free to browse our gallery");
+  }
 }
 bookNow.onclick = redirect;
+
+
+/*function goToGallery() {
+    let password = "test";
+    let userInput = prompt("Please enter the Password");
+    let result;
+  if (password === userInput) {
+    result = "Success";}
+    else {
+        result = "Please Try Again";
+    }
+    /*console.log(result);
+    return result;
+}
+
+function check () {
+    goToGallery();
+    if (goToGallery === "Success") {
+        window.location.href = "gallery.html";
+    }
+}
+galleryButton.onclick = check;*/
+
+let username = "user"
+let password = "admin";
+function enterPassword(userInput, truth){
+    if (userInput === truth){
+        return "success";
+    }
+    else {return "Please try again";}
+}
+
+function goNow() {
+    let result = enterPassword (prompt("Please enter the password"), password);
+    
+    if (result === "success"){
+        alert("Successful");
+        window.location.href = "gallery.html";
+    }
+    else {
+        alert("Wrong Password.  Please Try Again");
+    }
+}
+galleryButton.onclick = goNow;
