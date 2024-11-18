abe1d6c30b385e09bf9c8ad4a2d4aeb455412ea1
aphamil06 <aphamil06@gmail.com>
2024-11-18 14:43:28 -0600

    added object and stringified the object

--- contact.js
+++ contact.js
@@ -10,14 +10,20 @@ function processForm(event){
     const input1 = form[1];
     const value = input.value;
     const value1 = input1.value;
+    let user = {
+        name: value,
+        email: value1,
+    };
+
     output(`Your Name is ${value} <br>`);
     output(`Your Email Address is ${value1}<br>`);
     setTimeout(serverSimulation,5000);
     output("Your form is being processed...<br>");
+    output (JSON.stringify(user));
 }
 
 function serverSimulation(){
-output("Your form has successfully been processed. ");
+output("<br>Your form has successfully been processed. ");
 }
 
 function processAppointment(event){
@@ -30,7 +36,13 @@ function processAppointment(event){
     const dateValue =date.value;
     const phone = form[2];
     const phoneValue = phone.value;
+    let user = {
+        name: nameValue,
+        date: dateValue,
+        Phone: phoneValue,
+    };
     setTimeout(serverSimulation,5000);
-    output(`<br> Hi ${nameValue}, the date and time for your scheduled phone appointment will be ${dateValue}.  We will call you at ${phoneValue}.  We look forward to speaking with you then. Please wait while we finish processing your appointment....<br>`);
+    output(`<br> Hi ${nameValue}, the date and time for your scheduled phone appointment will be ${dateValue}. <br> We will call you at ${phoneValue}. <br> We look forward to speaking with you then.<br> Please wait while we finish processing your appointment....<br>`);
+    output(JSON.stringify (user));
 }
 
